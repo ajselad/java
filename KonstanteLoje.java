f8a67a123a392eefce6480009d4f315cff94b7c6
Ajsela <132933809+ajselad@users.noreply.github.com>
2024-06-23 14:09:04 +0200

    Delete KonstanteLoje.java

--- KonstanteLoje.java
+++
@@ -1,8 +0,0 @@
-public class KonstanteLoje {
-    public static int RRESHTA = 11;
-    public static int KOLONA = 21;
-    public static int THESARE_TOTALE = 10;
-    public final static int NEW_GAME = 0;
-    public final static int SAVED_GAME = 1;
-}
-
